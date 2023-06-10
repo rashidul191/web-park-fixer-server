fbe5b9b70897dbbb529d2fffc41095068489383d
rashidul191 <rashidul.191cse.gub@gmail.com>
2023-06-10 09:52:55 +0600

    change app and server file path

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-
 const app = require("./app");
-const { connectToServer } = require("../middleware/dbConnection");
+const { connectToServer } = require("./middleware/dbConnection");
 require("dotenv").config();
 const port = process.env.PORT || 5050;
 
-
 // database connect to server
-connectToServer() // import here connectToServer here
+connectToServer(); // import here connectToServer here
 app.listen(port, () => {
   console.log(`listen port at : ${port}`);
 });
